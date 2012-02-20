ad378eecd5c0f5930fd042b14abc00f1b1f73273
Dustin Sallings <dustin@spy.net>
2012-02-20 14:20:32 -0800

    Pull view data from couchdb into a CSV file.

---
+++ run2csv/run2csv.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"code.google.com/p/dsallings-couch-go"
+)
+
+var couchUrl *string = flag.String("couch", "http://localhost:5984/stats",
+	"Couch DB.")
+
+type ResultRow struct {
+	Key   []interface{}
+	Value float64
+}
+
+type Results struct {
+	Rows []ResultRow
+}
+
+func maybefatal(str string, err error) {
+	if err != nil {
+		log.Fatalf("%s: %v", str, err)
+	}
+}
+
+func getResults(db couch.Database, run, which string) {
+	query_results := Results{}
+
+	err := db.Query("_design/statdata/_view/"+which,
+		map[string]interface{}{
+			"reduce":    false,
+			"start_key": "[" + run + "]",
+		},
+		&query_results)
+	maybefatal("Error querying couchdb", err)
+
+	if len(query_results.Rows) < 1 {
+		log.Printf("WARNING:  No %v for %v", which, run)
+		return
+	}
+
+	fn := run + "-" + which + ".csv"
+	fout, err := os.Create(fn)
+	maybefatal("Error creating csv", err)
+	defer fout.Close()
+	log.Printf("Writing CSV at %v", fn)
+	c := csv.NewWriter(fout)
+
+	header := []string{}
+	for n := range query_results.Rows[0].Key {
+		header = append(header, fmt.Sprintf("k%d", n+1))
+	}
+	header = append(header, "value")
+	maybefatal("Error writing csv header", c.Write(header))
+
+	for _, r := range query_results.Rows {
+		out := make([]string, 0, len(header))
+		for _, v := range r.Key[1:] {
+			out = append(out, fmt.Sprintf("%v", v))
+		}
+		out = append(out, fmt.Sprintf("%v", r.Value))
+
+		maybefatal("Error writing csv row", c.Write(out))
+	}
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() < 1 {
+		log.Printf("Need the name of the run to grab.")
+		flag.Usage()
+		os.Exit(1)
+	}
+	run := flag.Arg(0)
+
+	db, err := couch.Connect(*couchUrl)
+	maybefatal("Error connecting to couchdb", err)
+
+	getResults(db, run, "kvtimings")
+	getResults(db, run, "timings")
+	getResults(db, run, "toplevel")
+}
